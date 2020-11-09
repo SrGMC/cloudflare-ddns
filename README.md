d6e29d3219ca1475a4d19f425a61cfe56e6dea0a
Álvaro Galisteo <alvarogalisteo@outlook.es>
2020-11-09 23:06:54 +0100

    Added config file and fixed some bugs

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Update DNS entries in Cloudflare and use Cloudflare as a DDNS provider.
 1. Install [Node.js](https://nodejs.org/en/)
 2. Clone this project with `git clone https://github.com/SrGMC/cloudflare-ddns.git`
 3. Install the requiered dependencies with `npm install`.
-4. Edit `index.js` and change the following:
-    - `ZONE_ID`: Zone ID assigned to the Zone you want to update.
-    - `ZONE_NAME`: The name of the Zone you want to update.
-    - `AUTH_EMAIL`: The email used to login into your Cloudflare account.
-    - `AUTH_KEY`: Your global API key. This can be obtained in **My Profile** > **API Tokens** > **Global API Key**.
+4. Edit `config.json` and change the following:
+    - `zone_id`: Zone ID assigned to the Zone you want to update.
+    - `zone_name`: The name of the Zone you want to update.
+    - `auth_email`: The email used to login into your Cloudflare account.
+    - `auth_key`: Your global API key. This can be obtained in **My Profile** > **API Tokens** > **Global API Key**.
     - `records`: The DNS records you want to update. You can add as many as you want:
         - `@` is the root domain.
 5. Run the script with `./update-dns.sh`
@@ -27,12 +27,6 @@ Update DNS entries in Cloudflare and use Cloudflare as a DDNS provider.
 - `-b`: Bypass proxy: set all DNS records as non proxied. Useful for renewing certificates.
 - `-h`: Show this help message.
 
-### Cron
+### Schedule
 
-You can run this script every 30 minutes by adding the following line in `crontab -e`:
-
-```cron
-*/30 * * * * /path/to/cloudflare-ddns/update-dns.sh >/dev/null 2>&1
-```
-
-Change `/path/to/cloudflare-ddns` accordingly
+This script is set up to update DNS records every 30 min.
--- index.js
+++ index.js
@@ -1,122 +1,136 @@
-const request = require('sync-request');
+const request = require("sync-request");
+const fs = require("fs");
 
-/*
- * CHANGE THIS
- */
+var ZONE_ID = "";
+var ZONE_NAME = "";
+var AUTH_EMAIL = "";
+var AUTH_KEY = "";
+var records = {};
 
-const ZONE_ID = '[Your Zone ID]';
-const ZONE_NAME = '[Your Zone Name]'
-const AUTH_EMAIL = '[Your Email]';
-const AUTH_KEY = '[Your Cloudflare Global API]';
-var records = {
-    '@': {'type': 'A', 'proxied': true},
-    'subdomain1': {'type': 'A', 'proxied': false},
-    'subdomain2': {'type': 'A', 'proxied': true},
+if (process.argv[2] === "-h") {
+    showHelp();
+} else if (process.argv.length === 3 && process.argv[2] !== "-b") {
+    console.log("Error: Unrecognized argument.\n");
+    showHelp();
+} else if (process.argv.length >= 4) {
+    console.log("Error: Invalid number of arguments.\n");
+    showHelp();
 }
 
-/*
- * END CHANGE THIS
- */
-
-if (process.argv[2] === '-h'){
-    showHelp();
-} else if (process.argv.length === 3 && process.argv[2] !== '-b'){
-    console.log('Error: Unrecognized argument.\n');
-    showHelp();
-} else if (process.argv.length >= 4){
-    console.log('Error: Invalid number of arguments.\n');
-    showHelp();
-} 
-
-function showHelp(){
-    console.log('Node.js script to update Cloudflare DNS records');
-    console.log('Usage: ./update-dns.sh [-b | -h]\n');
-    console.log('Optional arguments:')
-    console.log(' -b          Bypass proxy: set all DNS records as non proxied.');
-    console.log('             Useful for renewing certificates.');
-    console.log(' -h          Show this help message');
+function showHelp() {
+    console.log("Node.js script to update Cloudflare DNS records");
+    console.log("Usage: ./update-dns.sh [-b | -h]\n");
+    console.log("Optional arguments:");
+    console.log(" -b          Bypass proxy: set all DNS records as non proxied.");
+    console.log("             Useful for renewing certificates.");
+    console.log(" -h          Show this help message");
     process.exit(1);
 }
 
 // Global variables
-var IP = '';
-var bypass = process.argv[2] === '-b' ? true : false;
-const API_ENDPOINT = 'https://api.cloudflare.com/client/v4/zones/' + ZONE_ID;
+var IP = "";
+var bypass = process.argv[2] === "-b" ? true : false;
+const API_ENDPOINT = "https://api.cloudflare.com/client/v4/zones" + ZONE_ID;
 
-const recordOptions = {
-    headers: {
-        'X-Auth-Email': AUTH_EMAIL,
-        'X-Auth-Key': AUTH_KEY
-    }
-};
-
+var recordOptions = {};
 
 // Get public IP
 function getIP() {
-    var res = request('GET', 'https://canihazip.com/s');
+    var res = request("GET", "https://api.ipify.org?format=json");
     if (res.statusCode == 200) {
-        IP = res.getBody();
+        IP = JSON.parse(res.getBody()).ip;
         //console.log("Public IP: " + IP);
     } else {
-        console.log('Error: An error has occured while obtaining public IP');
+        console.log("Error: An error has occured while obtaining public IP");
         process.exit(1);
     }
 }
 
-
 // Get DNS records
 function getRecords() {
-    var res = request('GET', API_ENDPOINT + '/dns_records', recordOptions);
+    recordOptions = {
+        headers: {
+            "X-Auth-Email": AUTH_EMAIL,
+            "X-Auth-Key": AUTH_KEY,
+        },
+    };
+
+    var res = request("GET", API_ENDPOINT + "/" + ZONE_ID + "/dns_records", recordOptions);
     if (res.statusCode == 200) {
         var result = JSON.parse(res.getBody()).result;
+
         for (var i = 0; i < result.length; i++) {
-            var name = result[i].name.replace(ZONE_NAME, '').replace('.', '');
-            if (name === ''){
-                name = '@';
+            var name = result[i].name.replace(ZONE_NAME, "").replace(".", "");
+            if (name === "") {
+                name = "@";
             }
-            records[name].id = result[i].id;
+
+            if (records[name]) {
+                records[name].id = result[i].id;
+            }
         }
         //console.log(records);
     } else {
-        console.log('Error: An error has occured while obtaining DNS records');
-        console.log('See more below:');
-        console.log('Status Code:' + response.statusCode);
+        console.log("Error: An error has occured while obtaining DNS records");
+        console.log("See more below:");
+        console.log("Status Code:" + res.statusCode);
         console.log(JSON.parse(res.getBody()));
         process.exit(1);
     }
 }
 
-function updateRecords(){
+function updateRecords() {
     var recordNames = Object.keys(records);
     for (var i = 0; i < recordNames.length; i++) {
         var name = recordNames[i];
-        var endpoint = API_ENDPOINT + '/dns_records/' + records[name].id;
+        var endpoint = API_ENDPOINT + "/" + ZONE_ID + "/dns_records/" + records[name].id;
 
         options = {
             headers: {
-                'X-Auth-Email': AUTH_EMAIL,
-                'X-Auth-Key': AUTH_KEY,
+                "X-Auth-Email": AUTH_EMAIL,
+                "X-Auth-Key": AUTH_KEY,
             },
             json: {
                 type: records[name].type,
-                name: name == '@' ? '' : name,
-                content: IP + '',
+                name: name == "@" ? "" : name,
+                content: IP + "",
                 ttl: 1,
-                proxied: !bypass ? records[name].proxied : false
-            }
-        }
-        var res = request('PUT', endpoint, options);
+                proxied: !bypass ? records[name].proxied : false,
+            },
+        };
+        var res = request("PUT", endpoint, options);
         if (res.statusCode !== 200) {
-            console.log('Error: An error has occured while updating ' + name);
+            console.log("Error: An error has occured while updating " + name);
             console.log(JSON.parse(res.getBody()));
         }
     }
 }
 
-function main(){
+function main() {
     getIP();
     getRecords();
     updateRecords();
+    console.log("Updated successfully");
+
+    setInterval(function () {
+        getIP();
+        getRecords();
+        updateRecords();
+        console.log("Updated successfully");
+    }, 30000);
 }
 
-main();
+fs.readFile("./config.json", "utf8", function (err, data) {
+    if (err) {
+        process.exit(1);
+    }
+
+    var config = JSON.parse(data);
+    ZONE_ID = config.zone_id;
+    ZONE_NAME = config.zone_name;
+    AUTH_EMAIL = config.auth_email;
+    AUTH_KEY = config.auth_key;
+    records = config.records;
+
+    main();
+});
